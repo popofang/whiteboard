337ffcc4b6b909a5f7bdead05bf2345c8730942d
Shelly <zhangyuyue@coocaa.com>
2016-04-20 14:34:33 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+//app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port);
+
+console.log('imooc started on port ' + port);
+
+//index page
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'Whiteboard'
+	})
+});
