85e83a11f94a8780acc36c2b106d893676b5ad6d
Shelly <zhangyuyue@coocaa.com>
2016-05-06 01:14:24 +0800

    node-tesseract

--- app.js
+++ app.js
@@ -25,3 +25,14 @@ socketio.sockets.on('connection', function(socket) {
     socket.broadcast.emit('draw', data);
   });
 });
+
+var tesseract = require('node-tesseract');
+
+// Recognize text of any language in any format
+tesseract.process(__dirname + '/test.png',function(err, text) {
+  if(err) {
+      console.error(err);
+  } else {
+      console.log(text);
+  }
+});
